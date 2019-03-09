a832fd87b99fdf64f8d64f888166392b5f99dc25
Martin Alonso <malonso87@gmail.com>
2019-03-08 19:05:31 -0500

    Project data and initial notebooks

---
+++ Project/TweetLocation.py
@@ -0,0 +1,21 @@
+import tweepy 
+import pandas as pd 
+
+class tweet_location:
+    """
+    Obtains tweet location by passing tweet ids through the parser.
+    """
+
+    def __init__(self, id):
+        self.id = id
+
+    def get_location(id):
+        try:
+            tweet_status = api.get_status(id)
+            tweet_location = tweet_status.user.location
+            tweet_data = [id, tweet_location]
+            return(tweet_data)
+        except:
+            print("Tweet ID not found")
+    
+    
